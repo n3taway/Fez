7d2f5b28583c4cce64bdc613bc27860c2b87c02b
n3taway <n3taway@gmail.com>
2020-09-29 09:45:42 +0800

    feat: getAllFiles

--- fez-i18n-coverage/src/extension.ts
+++ fez-i18n-coverage/src/extension.ts
@@ -1,27 +1,50 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as fs from 'fs-extra';
+import * as path from 'path';
+
+const fileSuffix = ['.js', '.jsx', '.ts', '.tsx'];
+const ignoreDir = ['locales','.umi'];
+
+
+function getAllFiles(dir: string): Array<string> {
+	return fs.readdirSync(dir).reduce((files: Array<string>, file) => {
+		const name = path.join(dir, file);
+		const isDirectory = fs.statSync(name).isDirectory();
+		// 忽略指定目录下的文件
+		if (isDirectory && ignoreDir.includes(file)) {
+			return [...files];
+		}
+		// 过滤.d.ts
+		if (!isDirectory && /\.d\.ts$/.test(file)) {
+			return [...files];
+		}
+		// 过滤文件类型
+		if (!isDirectory && !fileSuffix.includes(path.extname(file))) {
+			return [...files];
+		}
+		return isDirectory ? [...files, ...getAllFiles(name)] : [...files, name];
+	}, []);
+}
+
+
+function checkCoverage() {
+	const rootDirPath = `${vscode.workspace.rootPath}/src`;
+
+	const allFiles = getAllFiles(rootDirPath);
+
+	console.log('fez-i18n-coverage is now active!');
+}
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "fez-i18n-coverage" is now active!');
+	console.log('fez-i18n-coverage is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('fez-i18n-coverage.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from fez-i18n-coverage!');
-	});
-
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(vscode.commands.registerCommand('fez-i18n-coverage.checkCoverage', checkCoverage));
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
